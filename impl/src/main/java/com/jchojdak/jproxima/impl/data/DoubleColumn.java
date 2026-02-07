1a72914cc06c34a1aebadcae19c2a73c7f95bcb4
Jakub Chojdak <jchojdak@gmail.com>
2026-02-07 20:50:12 +0100

    feat: add DoubleColumn

---
+++ impl/src/main/java/com/jchojdak/jproxima/impl/data/DoubleColumn.java
@@ -0,0 +1,34 @@
+package com.jchojdak.jproxima.impl.data;
+
+import com.jchojdak.jproxima.data.DataType;
+
+import java.util.BitSet;
+
+final class DoubleColumn extends BaseColumn {
+
+    private final double[] data;
+
+    DoubleColumn(String name, double[] data, BitSet nullMask) {
+        super(name, DataType.DOUBLE, data.length, nullMask);
+        this.data = data;
+    }
+
+    @Override
+    public Object get(int index) {
+        return isNull(index) ? null : data[index];
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = isNull(i) ? data[i] : null;
+        }
+        return result;
+    }
+
+    @Override
+    protected String valueToString(int index) {
+        return isNull(index) ? "null" : String.valueOf(data[index]);
+    }
+}
