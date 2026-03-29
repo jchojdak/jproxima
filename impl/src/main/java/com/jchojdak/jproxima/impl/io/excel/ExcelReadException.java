81349911b8e05bdfdbfc38db2e305a899eabca05
Jakub Chojdak <jchojdak@gmail.com>
2026-03-29 21:29:25 +0200

    feat: add custom exception

---
+++ impl/src/main/java/com/jchojdak/jproxima/impl/io/excel/ExcelReadException.java
@@ -0,0 +1,7 @@
+package com.jchojdak.jproxima.impl.io.excel;
+
+public class ExcelReadException extends RuntimeException {
+    public ExcelReadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
